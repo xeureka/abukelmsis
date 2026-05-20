16b26633adb701f737215b5b71e83f89315593c2
Nardos1624 <nardosbizunsh@gmail.com>
2026-05-20 07:49:26 +0300

    updating vercel

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -76,7 +76,7 @@ function Home() {
             </motion.div>
           </motion.div>
 
-          <motion.div 
+          <motion.div
             initial={{ opacity: 0, scale: 0.9 }}
             animate={{ opacity: 1, scale: 1 }}
             transition={{ duration: 1.2, ease: "easeOut" }}
